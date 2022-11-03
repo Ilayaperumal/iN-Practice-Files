013f6e3db13b51258ebcad9c13c272e337b551af
ILAYAPERUMAL K <113359155+Ilayaperumal@users.noreply.github.com>
2022-11-03 23:37:52 +0530

    Add files via upload

---
+++ Day 12 my_module.py
@@ -0,0 +1,8 @@
+data = {"name": "ILAYA", 
+"course" : ["data science", "block chain", "big data"], "greeting" : "greeting from ineuron"
+}
+
+def get_course():
+	return data["course"]
+def greetings():
+	return data['greeting']
---
+++ Day 12 test2.py
@@ -0,0 +1,15 @@
+def test(a,b):
+	return a+b
+
+def test1(b,c):
+	""""THIS IS A FUNCTIONN WHICH WILL RETURN MUL OF 2 NUMS"""
+	return b*c
+
+def test3(b,n):
+	return b/n
+
+def test4():
+	print("this is my code written in module test 2")
+
+def test5(b,n):
+	return b-n
